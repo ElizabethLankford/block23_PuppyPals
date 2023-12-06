c2f6527f501272badec7e8ebb88c869aa10b8f7e
ElizabethLankford <elizabethlankford1@gmail.com>
2023-12-06 14:21:49 -0600

    added select featured pup functionality

--- Workshop-PuppyPals/src/App.jsx
+++ Workshop-PuppyPals/src/App.jsx
@@ -4,12 +4,33 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       {puppies.map((pup) => {
-        return <p key={pup.id}>{pup.name}</p>;
+        return (
+          <p
+            key={pup.id}
+            onClick={() => {
+              setFeatPupId(pup.id);
+            }}
+          >
+            {pup.name}
+          </p>
+        );
       })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
